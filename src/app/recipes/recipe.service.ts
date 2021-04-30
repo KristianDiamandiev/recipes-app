db13109f82fd2059df1cefa9ddb8998cc255ba5b
hakokoha <kriskosi123@gmail.com>
2021-04-30 13:35:46 +0300

    storing and fetching data from firebase

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,25 +10,31 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe', 'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French fries', 20)
-      ]),
-    new Recipe(
-      'Another Test Recipe',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1),
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'A Test Recipe', 'This is simply a test',
+  //     'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French fries', 20)
+  //     ]),
+  //   new Recipe(
+  //     'Another Test Recipe',
+  //     'This is simply a test',
+  //     'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+  //     [
+  //       new Ingredient('Buns', 2),
+  //       new Ingredient('Meat', 1),
+  //     ])
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
+
+import { Recipe } from "../recipes/recipe.model";
+import { RecipeService } from "../recipes/recipe.service";
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipesService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipesService.getRecipes();
+    this.http
+      .put('https://ng-recipe-book-42315-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', recipes)
+      .subscribe(response => {
+        console.log(response)
+      });
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>('https://ng-recipe-book-42315-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ingredients: [],
+            ...recipe,
+          }
+        })
+      }))
+      .subscribe(recipes => {
+        this.recipesService.setRecipes(recipes);
+      })
+  }
+}
