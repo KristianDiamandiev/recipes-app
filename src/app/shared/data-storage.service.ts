e1e27cb8c5e0026b0157881e7ccee1f6a052cea4
hakokoha <kriskosi123@gmail.com>
2021-06-15 12:25:33 +0300

    sending auth token to request with interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,18 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map, tap } from "rxjs/operators";
+import { map, tap, take, exhaustMap } from "rxjs/operators";
 
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipesService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipesService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipesService.getRecipes();
@@ -20,18 +25,20 @@ export class DataStorageService {
 
   fetchRecipes() {
     return this.http
-      .get<Recipe[]>('https://ng-recipe-book-42315-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
-      .pipe(map(recipes => {
-        return recipes.map(recipe => {
-          return {
-            ingredients: [],
-            ...recipe,
-          };
-          });
-        }),
-        tap(recipes => {
-          this.recipesService.setRecipes(recipes);
-        })
-      )
+      .get<Recipe[]>(
+        'https://ng-recipe-book-42315-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+      ).pipe(
+          map(recipes => {
+            return recipes.map(recipe => {
+              return {
+                ingredients: [],
+                ...recipe,
+              };
+            });
+          }),
+          tap(recipes => {
+            this.recipesService.setRecipes(recipes);
+          })
+        )
   }
 }
