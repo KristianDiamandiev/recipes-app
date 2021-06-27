53eb7113638a7b971e39a04521f7cd410fdef51b
hakokoha <kriskosi123@gmail.com>
2021-06-27 14:15:38 +0300

    recipes resolver, fixing error on reload

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -6,6 +6,7 @@ import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RecipesComponent } from "./recipes.component";
+import { RecipesResolverService } from "./recipes-resolver.service";
 
 const routes: Routes = [
   { path: '', component: RecipesComponent,
@@ -13,8 +14,8 @@ const routes: Routes = [
   children: [
     { path: '', component: RecipeStartComponent },
     { path: 'new', component: RecipeEditComponent},
-    { path: ":id", component: RecipeDetailComponent },
-    { path: ':id/edit', component: RecipeEditComponent },
+    { path: ":id", component: RecipeDetailComponent, resolve: [RecipesResolverService] },
+    { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] },
     ]
   },
 ]
